eb30a07c28e9869a424411a70e68fa628618d4a9
dmayo2 <dmayo2@mit.edu>
2015-01-11 03:25:17 -0500

    parse all messages except keyvalue and requestkeyvalues packets

--- Java Bot/src/bot/Bot.java
+++ Java Bot/src/bot/Bot.java
@@ -1,5 +1,5 @@
 package bot;
-import java.util.HashMap;
+
 import java.util.Map;
 
 import cards.Card;
@@ -16,39 +16,9 @@ public class Bot {
     }
     
     
-    public String getAction(String[] actionMessage){
-        parseAction(actionMessage);
-        
+    public String getAction(Map<String, String> gETACTION){
         return "CHECK";
     }
     
-    public void parseAction(String[] actionMessage){
-        //GETACTION potSize numBoardCards [boardCards] [stackSizes] numActivePlayers [activePlayers] numLastActions [lastActions] numLegalActions [legalActions] timebank
-        Map<String, String> arg = new HashMap<String, String>();
-        int i=1;
-        arg.put("potSize", actionMessage[i++]);
-        arg.put("numBoardCards", actionMessage[i++]);
-        for (int x=0;x<Integer.parseInt(arg.get("numBoardCards"));x++){
-            arg.merge("boardCards", actionMessage[i++], (value, newValue) -> value.concat(" ".concat(newValue)));
-        }
-        //not sure if numActivePlayers is actually the number of actualPlayers parameters
-        arg.put("numActivePlayers", actionMessage[i++]);
-        for (int x=0;x<3;x++){
-            arg.merge("activePlayers", actionMessage[i++], (value, newValue) -> value.concat(" ".concat(newValue)));
-        }
-        
-        arg.put("numLastActions", actionMessage[i++]);
-        for (int x=0;x<Integer.parseInt(arg.get("numLastActions"));x++){
-            arg.merge("lastActions", actionMessage[i++], (value, newValue) -> value.concat(" ".concat(newValue)));
-        }
-        
-        arg.put("numLegalActionss", actionMessage[i++]);
-        for (int x=0;x<Integer.parseInt(arg.get("numLegalActions"));x++){
-            arg.merge("legalActions", actionMessage[i++], (value, newValue) -> value.concat(" ".concat(newValue)));
-        }
-        
-        arg.put("timebank", actionMessage[i++]);
-
-    }
 
 }
--- Java Bot/src/runner/Communicator.java
+++ Java Bot/src/runner/Communicator.java
@@ -3,6 +3,8 @@ package runner;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
 
 import bot.Bot;
 
@@ -25,6 +27,93 @@ public class Communicator {
         this.inStream = input;
     }
     
+    public Map<String, String> parseGetAction(String[] actionMessage){
+        //GETACTION potSize numBoardCards [boardCards] [stackSizes] numActivePlayers [activePlayers] numLastActions [lastActions] numLegalActions [legalActions] timebank
+        Map<String, String> arg = new HashMap<String, String>();
+        int i=1;
+        arg.put("potSize", actionMessage[i++]);
+        arg.put("numBoardCards", actionMessage[i++]);
+        for (int x=0;x<Integer.parseInt(arg.get("numBoardCards"));x++){
+            arg.merge("boardCards", actionMessage[i++], (value, newValue) -> value.concat(" ".concat(newValue)));
+        }
+        //not sure if numActivePlayers is actually the number of actualPlayers parameters
+        arg.put("numActivePlayers", actionMessage[i++]);
+        for (int x=0;x<3;x++){
+            arg.merge("activePlayers", actionMessage[i++], (value, newValue) -> value.concat(" ".concat(newValue)));
+        }
+        
+        arg.put("numLastActions", actionMessage[i++]);
+        for (int x=0;x<Integer.parseInt(arg.get("numLastActions"));x++){
+            arg.merge("lastActions", actionMessage[i++], (value, newValue) -> value.concat(" ".concat(newValue)));
+        }
+        
+        arg.put("numLegalActionss", actionMessage[i++]);
+        for (int x=0;x<Integer.parseInt(arg.get("numLegalActions"));x++){
+            arg.merge("legalActions", actionMessage[i++], (value, newValue) -> value.concat(" ".concat(newValue)));
+        }
+        
+        arg.put("timebank", actionMessage[i++]);
+        
+        return arg;
+    }
+    
+    public Map<String, String> parseNewGame(String[] message){
+        //NEWGAME yourName opp1Name opp2Name stackSize bb numHands timeBank
+        Map<String, String> arg = new HashMap<String, String>();
+        int i=1;
+        arg.put("yourName", message[i++]);
+        arg.put("opp1Names", message[i++]);
+        arg.put("opp2Name", message[i++]);
+        arg.put("stackSize", message[i++]);
+        arg.put("bb", message[i++]);
+        arg.put("numHands", message[i++]);
+        arg.put("timeBank", message[i++]);
+
+        return arg;
+    }
+    
+    public Map<String, String> parseNewHand(String[] message){
+        //NEWHAND handId seat holeCard1 holeCard2 [stackSizes] [playerNames] numActivePlayers [activePlayers] timeBank
+        Map<String, String> arg = new HashMap<String, String>();
+        int i=1;
+        arg.put("handId", message[i++]);
+        arg.put("seat", message[i++]);
+        arg.put("holeCard1", message[i++]);
+        for (int x=0;x<3;x++){
+            arg.merge("stackSizes", message[i++], (value, newValue) -> value.concat(" ".concat(newValue)));
+        }
+        for (int x=0;x<3;x++){
+            arg.merge("playerNames", message[i++], (value, newValue) -> value.concat(" ".concat(newValue)));
+        }
+        arg.put("numActivePlayers", message[i++]);
+        for (int x=0;x<3;x++){
+            arg.merge("activePlayers", message[i++], (value, newValue) -> value.concat(" ".concat(newValue)));
+        }
+        arg.put("timeBank", message[i++]);
+
+        return arg;
+    }
+    
+    public Map<String, String> parseHandOver(String[] message){
+        //HANDOVER [stackSizes] numBoardCards [boardCards] numLastActions [lastActions] timeBank
+        Map<String, String> arg = new HashMap<String, String>();
+        int i=1;
+        for (int x=0;x<3;x++){
+            arg.merge("stackSizes", message[i++], (value, newValue) -> value.concat(" ".concat(newValue)));
+        }
+        arg.put("numBoardCards", message[i++]);
+        for (int x=0;x<Integer.parseInt(arg.get("numBoardCards"));x++){
+            arg.merge("boardCards", message[i++], (value, newValue) -> value.concat(" ".concat(newValue)));
+        }
+        arg.put("numLastActions", message[i++]);
+        for (int x=0;x<Integer.parseInt(arg.get("numLastActions"));x++){
+            arg.merge("lastActions", message[i++], (value, newValue) -> value.concat(" ".concat(newValue)));
+        }
+        arg.put("timeBank", message[i++]);
+
+        return arg;
+    }
+    
     public void run() {
         String input;
         try {
@@ -44,12 +133,24 @@ public class Communicator {
                     // The engine will ignore all spurious packets you send.
                     // The engine will also check/fold for you if you return an
                     // illegal action.
-                    String action = bot.getAction(inputWords);
+                    
+                    Map<String,String> parsed = parseGetAction(inputWords);
+                    
+                    String action = bot.getAction(parsed);
                     outStream.println(action);
                 } else if ("REQUESTKEYVALUES".compareToIgnoreCase(word) == 0) {
                     // At the end, engine will allow bot to send key/value pairs to store.
                     // FINISH indicates no more to store.
                     outStream.println("FINISH");
+                } else if ("NEWGAME".compareToIgnoreCase(word) == 0) {
+                    Map<String,String> parsed = parseNewGame(inputWords);
+                    
+                } else if ("NEWGAME".compareToIgnoreCase(word) == 0) {
+                    Map<String,String> parsed = parseNewHand(inputWords);
+                    
+                } else if ("NEWGAME".compareToIgnoreCase(word) == 0) {
+                    Map<String,String> parsed = parseHandOver(inputWords);
+                    
                 }
             }
         } catch (IOException e) {
@@ -67,5 +168,7 @@ public class Communicator {
             e.printStackTrace();
         }
     }
+    
+    
     
 }
