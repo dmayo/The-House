6c219a4b2a3e226ad599a9d9b2d2708bf5700f67
dmayo2 <dmayo2@mit.edu>
2015-01-14 22:43:28 -0500

    automitic poker simulation runner scripts

--- Java Bot/test.sh
+++ Java Bot/test.sh
@@ -1,10 +1,12 @@
-max=10
+max=100
 function pause(){
    read -p "$*"
 }
+sleep 5
 for i in `seq 1 $max`
 do
 	echo $i
-    sh pokerbot.sh 3002
-	pause 'Press [Enter] key to continue...'
+    sh pokerbot.sh 3004
+	#pause 'Press [Enter] key to continue...'
+	sleep 3
 done
